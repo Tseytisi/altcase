8bb70da226c7d58d83f59469c9f6705e85d80f83
Matthijs Veldkamp <veldkamp.matthijs@gmail.com>
2023-05-02 00:13:37 +0200

    Greatly improved GUI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AltCase
 
-> JTC, April 2023
+> Tseytisi, April 2023
 
 Simple tool for converting text to alternating case. aLLoWiNg YoU tO EaSiLy wRiTe LiKe tHiS.
 
@@ -8,7 +8,35 @@ Works as both a command-line program, and as GUI application.
 
 Has a mode that ensures all occurrences of 'i' are lowercase, and all occurrences of 'L' are uppercase, to avoid confusion.
 
-### Upcoming features
+### How to install
+1. Clone the repository 
+```sh
+git clone https://github.com/Tseytisi/altcase
+```
+2. Ensure the Rustup package is installed, and that the proper toolchain is configured
+```sh
+rustup default stable
+```
+3. Inside the cloned repository, in the same folder where the `Cargo.toml` file is located, build the application
+```sh
+cargo build --release
+```
+4. The compiled binary should now be located inside the folder at `target/release/altcase`
+
+### How to run
+To get a help and usage screen from the application, type
+```sh 
+altcase --help
+```
+
+By default, the application runs in command-line mode, meaning no GUI will be shown or loaded.
+
+To launch the application with GUI, run
+```sh 
+altcase --gui
+```
+
+### Planned features
 - Allow input through pipe
 - Split input on newlines so each line gets the best mapping (in normal conversion mode)
 - Start with capital or non-capital letter in simple mode depending on the first character
--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,6 @@
 use gtk::prelude::*;
 use gtk::{ApplicationWindow, Application};
+use gtk::gdk_pixbuf::Pixbuf;
 use std::sync::Mutex;
 
 use crate::case_convert::convert;
@@ -24,58 +25,115 @@ pub(crate) fn start(app: &Application, settings: &Settings) {
 fn create_window(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
-        .default_width(400)
-        .default_height(600)
+        .default_width(600)
+        .default_height(400)
         .title("AltCase")
         .build();
 
-    let layout = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    layout.set_homogeneous(true);
-
-    let upper_half = gtk::Box::new(gtk::Orientation::Vertical, 0);
-    let bottom_half = gtk::Box::new(gtk::Orientation::Vertical, 0);
-
-    let top_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+    // Simple mode toggle
     let simple_mode_label = gtk::Label::builder()
         .label("Simple Mode")
         .build();
     let simple_mode_switch = gtk::Switch::builder()
-        .margin(10)
         .build();
+    let simple_mode_info_icon = gtk::Image::from_icon_name(Some("help-about"), gtk::IconSize::LargeToolbar);
+
+    // Simple mode container
+    let simple_mode_box = gtk::Box::new(gtk::Orientation::Horizontal, 10);
+    simple_mode_box.add(&simple_mode_switch);
+    simple_mode_box.add(&simple_mode_label);
+    simple_mode_box.add(&simple_mode_info_icon);
+    simple_mode_box.set_tooltip_text(
+        Some("When simple mode is disabled, certain letters (such as 'i' and 'L') \
+        will always be kept upper or lower case to avoid confusion."));
+
+    // Set state of simple mode switch
     {
         // Set initial state of the simple-mode switch
         let initial_mode = SIMPLE_MODE.lock()
             .expect("Error (gui.create_window.set_simple_mode_switch_state): Could not lock mutex");
         simple_mode_switch.set_state(*initial_mode);
     }
-    top_box.add(&simple_mode_switch);
-    top_box.add(&simple_mode_label);
-    top_box.set_tooltip_text(
-        Some("When simple mode is disabled, certain letters (such as 'i' and 'L') \
-        will be kept upper or lower case to avoid confusion."));
 
+    // Input and output text areas
     let tag_table = gtk::TextTagTable::new();
     let input_buffer = gtk::TextBuffer::new(Some(&tag_table));
     let input_text_field = gtk::TextView::builder()
         .expand(true)
-        .margin(10)
         .buffer(&input_buffer)
         .border_width(5)
+        .wrap_mode(gtk::WrapMode::WordChar)
         .build();
+    // Scroll box so the window won't resize if you add more text than fits the box
+    let input_scroll_box = gtk::ScrolledWindow::builder()
+        //.expand(true)
+        .build();
+    input_scroll_box.add(&input_text_field);
 
     let output_buffer = gtk::TextBuffer::new(Some(&tag_table));
     let output_text_field = gtk::TextView::builder()
         .expand(true)
         .editable(false)
-        .margin(10)
         .buffer(&output_buffer)
         .border_width(5)
+        .wrap_mode(gtk::WrapMode::WordChar)
+        .build();
+    let output_scroll_box = gtk::ScrolledWindow::builder()
+        //.expand(true)
+        .build();
+    output_scroll_box.add(&output_text_field);
+
+    // Convert button
+    let convert_button_box = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .valign(gtk::Align::Center)
         .build();
     let convert_button = gtk::Button::builder()
-        .label("Convert")
-        .margin(10)
+        .build();
+    convert_button_box.add(&convert_button);
+    if let Ok(image) = Pixbuf::from_file("img/arrow.png") {
+        let convert_arrow = gtk::Image::from_pixbuf(Some(&image));
+        convert_button.set_image(Some(&convert_arrow));
+        convert_button.set_always_show_image(true);
+    } else {
+        convert_button.set_label("Convert");
+    }
+
+    // Copy-to-clipboard button
+    let copy_to_clipboard_button = gtk::Button::builder()
+        .label("Copy to clipboard")
+        .build();
+    let copy_icon = gtk::Image::from_icon_name(Some("edit-copy"), gtk::IconSize::Button);
+    copy_icon.set_margin_end(5);
+    copy_to_clipboard_button.set_image(Some(&copy_icon));
+    copy_to_clipboard_button.set_always_show_image(true);
+
+    // Layout
+    let grid = gtk::Grid::builder()
+        .border_width(10)
+        .column_spacing(10)
+        .row_spacing(10)
         .build();
 
+    // To ensure the two columns are the same width
+    let text_size_group = gtk::SizeGroup::new(gtk::SizeGroupMode::Horizontal);
+    text_size_group.add_widget(&input_text_field);
+    text_size_group.add_widget(&output_text_field);
+    text_size_group.add_widget(&simple_mode_box); // these were messing up the width even
+    text_size_group.add_widget(&copy_to_clipboard_button); // when there was enough space
+
+    // Layout left
+    grid.attach(&simple_mode_box, 0, 0, 1, 1);
+    grid.attach(&input_scroll_box, 0, 1, 1, 1);
+
+    // Centre
+    grid.attach(&convert_button_box, 1, 1, 1, 1);
+
+    // Right side
+    grid.attach(&output_scroll_box, 2, 1, 1, 1);
+    grid.attach(&copy_to_clipboard_button, 2, 2, 1, 1);
+
+    // Button functionality
     convert_button.connect_clicked(move |_button| {
         // Get the current contents of the input text area
         let input_text = input_buffer.text(
@@ -102,29 +160,17 @@ fn create_window(app: &Application) {
         }
     });
 
-    let copy_to_clipboard_button = gtk::Button::builder()
-        .margin(10)
-        .label("Copy to clipboard")
-        .build();
-
     copy_to_clipboard_button.connect_clicked(|_button| {
         // Retrieve the last output from the mutex
         let last_output = LAST_OUTPUT.lock().expect("Error (ctc_button.click): Could not lock mutex");
         let current_text = String::from(&*last_output);
 
-        // Create an object that represents the default clipboard in X
+        // Create an object that represents the default clipboard
         let clipboard = gtk::Clipboard::get(&gtk::gdk::Atom::intern("CLIPBOARD"));
         clipboard.set_text(&current_text);
     });
 
-    upper_half.add(&top_box);
-    upper_half.add(&input_text_field);
-    upper_half.add(&convert_button);
-    bottom_half.add(&output_text_field);
-    bottom_half.add(&copy_to_clipboard_button);
-
-    layout.add(&upper_half);
-    layout.add(&bottom_half);
-    window.add(&layout);
+    // Finalising
+    window.add(&grid);
     window.show_all();
 }
